18667107420b4a1381751988e77496c9a7be67df
mike-wise <mwise@microsoft.com>
2020-04-04 22:06:03 +0200

    progress

--- fileclass.py
+++ fileclass.py
@@ -9,35 +9,77 @@ parser.add_argument('--sdir', type=str, default=".",   help='source directory',r
 
 args = parser.parse_args()
 
+extDicts = {}
 
-def get_files(base_dir):
+def digestEntry( entry:os.DirEntry ):
+    global extDicts
+    _,ext = os.path.splitext(entry.name)
+    if ext=="":
+        print(entry.path)
+    if not ext in extDicts:
+        extDicts[ext] = { "num":0, "bytes":0 }
+    extdict = extDicts[ext]
+    extdict["num"] += 1
+    extdict["bytes"] += entry.stat().st_size
+
+
+
+def getFiles(base_dir):
     for entry in os.scandir(base_dir):
         if entry.is_file():
             yield entry
         elif entry.is_dir():
-            yield from get_files(entry.path)
+            print(entry.name)
+            if not entry.name.endswith(".git"):
+                if not entry.name=="Library":
+                    yield from getFiles(entry.path)
+            else:
+                print("found .git")
         else:
             print(f"Neither a file, nor a dir: {entry.path}")
 
 def buildExtDicts(sdir):
-    dicts = {}
-    for entry in get_files(sdir):
+    global extDicts
+    lgg.info(f"buildExtDicts",lgg.cW)          
+    extDicts = {}
+    for entry in getFiles(sdir):
         if entry.is_dir():
-            print(f"dir:{entry.path}")
+            lgg.info(f"  dir:{entry.path}",lgg.cB)
         elif entry.is_file():
-            print(f"file:{entry.path}")
-           
+            # lgg.info(f"  file:{entry.path}",lgg.cC)
+            digestEntry(entry)
     return 
+
+def dumpExtDicts():
+    global extDicts
+    lgg.info(f"dumpExtDicts",lgg.cW)  
+    nfiles = 0
+    nbytes = 0        
+    for key in extDicts.keys():
+        extdict = extDicts[key]
+        nfilesext = extdict["num"]
+        nbytesext = extdict["bytes"]
+        nfiles += nfilesext
+        nbytes += nbytesext
+        mbytes = nbytesext/1e6
+        lgg.info(f"  {key} - num:{nfilesext}  bytes:{nbytesext} mb:{mbytes}",lgg.cW)
+    mbytes = nbytes / 1e6
+    lgg.info(f"totals - files{nfiles} bytes:{nbytes} mb:{mbytes}",lgg.cW)
+
 
 sdir = args.sdir
 
 lgg.info(f"FileClassing {sdir}",lgg.cY)
 
-start = timeit.timeit()
+stime = timeit.time.time()
 buildExtDicts(sdir)
+dumpExtDicts()
+
 # (ovfiles,ovbytes) = copyFromTo(sdir,ddir,execute)
-elap = timeit.timeit()-start 
+etime = timeit.time.time()
+elap = etime-stime 
 
 #exword = "" if execute else "Would have "
 #lgg.info(f"{exword} Overwritten files:{ovfiles}/{tfiles}  overwritenbytes:{ovbytes} secs:{round(elap,3)} ",lgg.cY)
+# print(f"stime:{stime} etime:{etime} elap:{elap}")
 lgg.info(f"file class done - secs:{round(elap,3)} ",lgg.cY)
